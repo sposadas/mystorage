b997e27561e9804e7a08cc708aa96737c5248acc
sposadas <sebastian.posadas@mercadolibre.com>
2021-08-02 19:00:30 -0300

    -

--- internal/product/repository.go
+++ internal/product/repository.go
@@ -1,11 +1,14 @@
 package product
 
 import (
+	"database/sql"
+	database "github.com/sposadas/mystorage/db"
 	"github.com/sposadas/mystorage/internal/domain"
+	"log"
 )
 
 type Repository interface {
-	Store(name, productType string, count int, price float64) (domain.Product, error)
+	Store(domain.Product) (domain.Product, error)
 	GetOne(id int) domain.Product
 	Update(product domain.Product) (domain.Product, error)
 	GetAll() ([]domain.Product, error)
@@ -18,16 +21,63 @@ func NewRepository() Repository {
 	return &repository{}
 }
 
-func (r *repository) Store(name, productType string, count int, price float64) (domain.Product, error) {
-	return domain.Product{}, nil
+const (
+	InsertProduct = "INSERT INTO products(name, type, count, price) VALUES ( ?, ?, ?, ?)"
+	GetOneProduct = "SELECT * FROM products WHERE id = ?"
+	UpdateProduct = "UPDATE products SET name = ?, type = ?, count = ?, price = ? WHERE id = ?"
+)
+
+func (r *repository) Store(product domain.Product) (domain.Product, error) {
+	db := database.StorageDB
+
+	stmt, err := db.Prepare(InsertProduct)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var result sql.Result
+	result, err = stmt.Exec(product.Name, product.Type, product.Count, product.Price)
+	if err != nil {
+		return domain.Product{}, err
+	}
+	insertId, _ := result.LastInsertId()
+	product.ID = int(insertId)
+
+	return product, nil
 }
 
 func (r *repository) GetOne(id int) domain.Product {
-	return domain.Product{}
+	var product domain.Product
+	db := database.StorageDB
+
+	rows, err := db.Query(GetOneProduct, id)
+	if err != nil {
+		log.Println(err.Error())
+		return product
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&product.Name, &product.Type, &product.Count, &product.Price); err != nil {
+			log.Println(err.Error())
+			return product
+		}
+	}
+	return product
 }
 
 func (r *repository) Update(product domain.Product) (domain.Product, error) {
-	return domain.Product{}, nil
+	db := database.StorageDB
+	stmt, err := db.Prepare(UpdateProduct)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Type, product.Count, product.Price, product.ID)
+	if err != nil {
+		return domain.Product{}, err
+	}
+	return product, nil
 }
 
 func (r *repository) GetAll() ([]domain.Product, error) {
